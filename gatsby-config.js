7abcda4ecada6c644b805c413ff8c72f3703b943
ActarusC <cadieuxleblanc@gmail.com>
2021-01-08 08:06:00 -0500

    corr chemin favicon et chg couleur

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#649c33`,
         display: `minimal-ui`,
-        icon: `src\images\favicon-32x32.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-transformer-json',
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: '#649c33',
       marginBottom: `1.45rem`,
     }}
   >
